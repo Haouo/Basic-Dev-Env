3b71eb04b382e407d3c9dce0d172e0272b27301b
Haouo <q18131500@gs.ncku.edu.tw>
2025-01-25 13:11:01 -0500

    fix: workspace volume

--- create.sh
+++ create.sh
@@ -52,5 +52,6 @@ docker run -d --net=host \
     --env DISPLAY=$DISPLAY \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume ~/.ssh:/home/ubuntu/.ssh \
+    --volume ${VOLUME_NAME}:/home/ubuntu/workspace \
     --name "${CONTAINER_NAME}-dev-container" \
     "${CONTAINER_NAME}-dev-environment:latest"
